4e65d0eb039a1fd56f1b1562bda5e5e757e8fe9b
faithgarza <faith.garza001@gmail.com>
2020-03-17 17:01:57 -0500

    progress

--- src/main/java/com/codeup/springblog/repositories/PostRepo.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepo.java
@@ -1,7 +1,9 @@
-//package repositories;
-//
-//import com.codeup.springblog.models.Post;
-//import com.codeup.springblog.repositories.AdRepo;
-//
-//public interface PostRepo extends AdRepo<Post, Long> {
-//}
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.controllers.PostController;
+import com.codeup.springblog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepo extends JpaRepository<Post, Long> {
+}
